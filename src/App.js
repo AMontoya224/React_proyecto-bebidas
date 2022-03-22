cf57883cbc278722c3a7bbd472937b4fed0c0d82
Andres Montoya Angulo <jmontoyaan@unsa.edu.pe>
2022-03-22 17:46:33 -0500

    Proyecto Bebidas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Bebida from './Components/Bebida/Bebida';
+import FormularioBebida from './Components/BebidaForm/BebidaForm';
+import axios from 'axios';
 
 function App() {
+  const [nombreBebida, setNombreBebida] = useState( '' );
+  const [listaBebidas, setListaBebidas] = useState( [] );
+
+  const buscarBebida = ( event ) => {
+    event.preventDefault();
+    setNombreBebida( (nombrePrev) => event.target.nombreBebida.value );
+  }
+
+  /*
+  useEffect( () => {
+    async function request(){
+      try{
+        let respuesta = await axios.get( `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nombreBebida}` );
+        setListaBebidas( (listaPrev) => respuesta.data.drinks );
+      }
+      catch( err ){
+        console.log( err );
+      }
+    }
+    request();
+  }, [nombreBebida] ); 
+  */
+
+  useEffect( () => {
+    axios.get( `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nombreBebida}` )
+      .then( respuesta => {
+        setListaBebidas( (listaPrev) => respuesta.data.drinks );
+      })
+      .catch( err => {
+        console.log( err );
+      });
+
+  }, [nombreBebida] );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormularioBebida buscarBebida={buscarBebida}/>
+      {
+        listaBebidas.map( (bebida, indice) => {
+          return (
+            <Bebida key={`bebida_`+indice} bebida={bebida} />
+          );
+        })
+      }
     </div>
   );
+
 }
 
 export default App;
---
+++ src/Components/BebidaForm/BebidaForm.js
@@ -0,0 +1,15 @@
+function FormularioBebida( props ){
+    return(
+        <form onSubmit={ (event) => props.buscarBebida( event )}>
+            <label htmlFor="nombreBebida">
+                Nombre de la bebida:
+            </label>
+            <input type="text" id="nombreBebida" />
+            <button type="submit">
+                Buscar
+            </button>
+        </form>
+    );
+}
+
+export default FormularioBebida;
